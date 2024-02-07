58fe3ab3c9e173bc058eec322255360e6ad09aa8
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-02-07 22:15:58 +0800

    setup spring framework

---
+++ src/test/java/com/like/minded/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.like.minded.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

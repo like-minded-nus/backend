db69fe3365eff0887f20eb8cdf687ee4a0a53731
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-02-18 02:31:48 +0800

    fix condition in findMatchesByProfileId repository

--- src/main/java/com/like/minded/backend/repository/match/MatchRepository.java
+++ src/main/java/com/like/minded/backend/repository/match/MatchRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Integer> {
     @Query("SELECT m FROM Match m " +
-            "WHERE m.profileId_1 = :profileId OR m.profileId_2 = :profileId " +
-            "AND m.like_1 = TRUE AND m.like_2 = TRUE")
+            "WHERE m.like_1 = TRUE AND m.like_2 = TRUE " +
+            "AND m.profileId_1 = :profileId OR m.profileId_2 = :profileId")
     List<Match> findMatchesByProfileId(Integer profileId);
 }

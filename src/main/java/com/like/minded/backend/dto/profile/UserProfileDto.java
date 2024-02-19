cb0ff6896d87043a141eee77a40d9075e89c26ad
gaychin <gcyap1227@gmail.com>
2024-02-19 21:37:04 +0800

    add bio

--- src/main/java/com/like/minded/backend/dto/profile/UserProfileDto.java
+++ src/main/java/com/like/minded/backend/dto/profile/UserProfileDto.java
@@ -19,6 +19,7 @@ public class UserProfileDto {
     private String gender;
     private LocalDate birthdate;
     private List<Integer> profilePassionList;
+    private String bio;
     private String image1;
     private String image2;
     private String image3;
--- src/main/java/com/like/minded/backend/vo/profile/GetProfileResponse.java
+++ src/main/java/com/like/minded/backend/vo/profile/GetProfileResponse.java
@@ -17,6 +17,7 @@ public class GetProfileResponse {
     private String displayName;
     private String gender;
     private LocalDate birthdate;
+    private String bio;
     private List <String> profilePassionList;
     private String image1;
     private String image2;

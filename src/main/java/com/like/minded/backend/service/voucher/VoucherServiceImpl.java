b2b792b0a7fd88649fbfe590580ebc41b3e08dc5
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-03-30 04:50:40 +0800

    remove unused import `VoucherServiceImpl`

--- src/main/java/com/like/minded/backend/service/voucher/VoucherServiceImpl.java
+++ src/main/java/com/like/minded/backend/service/voucher/VoucherServiceImpl.java
@@ -8,7 +8,6 @@ import com.like.minded.backend.repository.voucher.VoucherRepository;
 import com.like.minded.backend.vo.voucher.VoucherResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;

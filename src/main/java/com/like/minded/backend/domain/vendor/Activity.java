899c117534bc79f91b8a9c2b80664eaf1d3694f2
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-03-31 15:23:46 +0800

    add @Data to `UserRole` and `Activity` to cater for unit testing

--- src/main/java/com/like/minded/backend/domain/vendor/Activity.java
+++ src/main/java/com/like/minded/backend/domain/vendor/Activity.java
@@ -1,8 +1,10 @@
 package com.like.minded.backend.domain.vendor;
+
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Activity {

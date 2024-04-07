887df287c37a2b1379f4ea0a1b4854214dccfb94
yongxiliew <yongxiliew@hotmail.com>
2024-04-08 03:02:58 +0800

    upgrade to premium user

--- src/main/java/com/like/minded/backend/controller/user/UserController.java
+++ src/main/java/com/like/minded/backend/controller/user/UserController.java
@@ -3,6 +3,7 @@ package com.like.minded.backend.controller.user;
 import com.like.minded.backend.dto.user.UserDto;
 import com.like.minded.backend.dto.user.UserLoginDto;
 import com.like.minded.backend.dto.user.UserRegistrationDto;
+import com.like.minded.backend.dto.user.UserUpgradePremiumDto;
 import com.like.minded.backend.service.user.UserService;
 import com.like.minded.backend.vo.BaseResponse;
 import com.like.minded.backend.vo.user.UserResponse;
@@ -26,6 +27,11 @@ public class UserController {
     @PostMapping(path="/login")
     public ResponseEntity<BaseResponse<UserDto>> loginUser(@RequestBody UserLoginDto userLoginDto) {
         return userService.loginUser(userLoginDto);
+    }
+
+    @PutMapping(path="/upgradetopremium")
+    public ResponseEntity<BaseResponse<Boolean>> upgradeToPremium(@RequestBody UserUpgradePremiumDto userUpgradePremiumDto) {
+        return userService.upgradeToPremium(userUpgradePremiumDto);
     }
 }
 
